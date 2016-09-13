072afeab0c3380ded29b4c67356ae270573fc6da
hehuazhen <1007126208@qq.com>
2016-09-13 15:58:27 +0800

    repair some bugs

--- add_two_num.sh
+++ add_two_num.sh
@@ -3,4 +3,4 @@
 read -p "Please input the first number:" first
 read -p "Please input the second number:" second
 sum=$(($first+$second))
-echo -e "the result of $first + $second = $sum"
+echo "the result of $first + $second = $sum"
--- max_three_num.sh
+++ max_three_num.sh
@@ -5,7 +5,7 @@ read -p "the first number is:" n1
 read -p "the second number is:" n2
 read -p "the third number is:" n3
 let MAX=$n1
-if [ $n2 -ge $n1 }; then
+if [ $n2 -ge $n1 ]; then
     MAX=$n2
 fi
 if [ $n3 -ge $MAX ]; then
